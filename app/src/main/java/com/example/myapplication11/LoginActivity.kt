80e4d7b864bc28ec235c024e5513c340e0191dd5
Samsung <cemaky3>
2023-03-27 10:09:15 +0300

    added new activities

--- app/src/main/java/com/example/myapplication11/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication11/LoginActivity.kt
@@ -1,24 +1,43 @@
 package com.example.myapplication11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.core.widget.addTextChangedListener
+import com.example.myapplication11.databinding.ActivityLoginBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        Timber.d("Main","created")
-        val textInputEditText: TextInputEditText?= findViewById(R.id.password_field)
-        val txtInputlay: TextInputLayout?= findViewById(R.id.password_field)
-        textInputEditText?.addTextChangedListener {
-            if (it.toString().length<6)
-                txtInputlay?.error = "fdjhlkjdsfjblsdjf"
-        }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.loginBtn.setOnClickListener {
+            if (binding.passwordFieldEt.text.toString()
+                    .isNotEmpty() && binding.emailFieldEt.text.toString().isNotEmpty()
+            ) {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
+        val spannableString = SpannableString(binding.toRegActivity.text)
 
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+            }
+        }
+        spannableString.setSpan(clickableSpan.underlying,25,36, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.toRegActivity.text = spannableString
+        binding.toRegActivity.movementMethod = LinkMovementMethod.getInstance()
+    }
     }
--- app/src/main/java/com/example/myapplication11/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication11/RegisterActivity.kt
@@ -1,11 +1,62 @@
 package com.example.myapplication11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import com.example.myapplication11.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.regBtn.setOnClickListener {
+            if(checkFields()){
+            val intent = Intent(this,MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            }
+        }
+        val spannableString = SpannableString(binding.toLoginActivity.text)
+
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            }
+        }
+        spannableString.setSpan(clickableSpan.underlying,22,27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.toLoginActivity.text = spannableString
+        binding.toLoginActivity.movementMethod = LinkMovementMethod.getInstance()
+
+    }
+    private fun checkFields(): Boolean {
+        return when {
+            binding.nameFieldEt.text.toString().length < 3 -> {
+                binding.nameField.error = "Имя не короче 3 символов"
+                false
+            }
+            !binding.emailFieldEt.text.toString().contains('@') -> {
+                binding.emailField.error = "Необходим символ \"@\""
+                false
+            }
+            binding.emailFieldEt.text.toString().length < 5 -> {
+                binding.passwordField.error = "Минимум 5 символов"
+                false
+            }
+            binding.passwordFieldEt.text.toString().length < 6 -> {
+                binding.passwordField.error = "Пароль не короче 6 символов"
+                false
+            }
+            else -> true
+        }
     }
 }
